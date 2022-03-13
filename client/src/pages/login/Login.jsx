501017f4318a223f2c2b12d19389f1def4c6b6f0
Raouf Satto <64332249+RaoufSEZAR@users.noreply.github.com>
2022-03-13 22:18:07 +0300

    Update Login.jsx

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -21,9 +21,9 @@ export default function Login() {
     <div className="login">
       <div className="loginWrapper">
         <div className="loginLeft">
-          <h3 className="loginLogo">Lamasocial</h3>
+          <h3 className="loginLogo">Saraha Social Media</h3>
           <span className="loginDesc">
-            Connect with friends and the world around you on Lamasocial.
+            Connect with friends and the world around you on Saraha Social Media.
           </span>
         </div>
         <div className="loginRight">
